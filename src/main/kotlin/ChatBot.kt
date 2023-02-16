a5bcc79440bdae27ae94cc172a8b103e13a96091
perepelochka <o.o.perepelytsia@student.khai.edu>
2023-02-16 16:16:19 +0200

    1 task

---
+++ src/main/kotlin/ChatBot.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    //1 задание
+    println("Hello!My name is Kotlin_Bot.\n" +
+            "Please, remind me your name.")
+
+    print(" ")
+    val name = readLine()
+    println("What a great name you have, $name!")
+
+
+}
+
+
+
