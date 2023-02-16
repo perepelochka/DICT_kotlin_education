58a9c93fe886174a7bf7a0e8204d4eef0521e420
perepelochka <o.o.perepelytsia@student.khai.edu>
2023-02-16 16:21:05 +0200

    2 task

--- src/main/kotlin/ChatBot.kt
+++ src/main/kotlin/ChatBot.kt
@@ -1,6 +1,7 @@
 import java.util.*
 
 fun main(args: Array<String>) {
+
     //1 задание
     println("Hello!My name is Kotlin_Bot.\n" +
             "Please, remind me your name.")
@@ -8,6 +9,15 @@ fun main(args: Array<String>) {
     print(" ")
     val name = readLine()
     println("What a great name you have, $name!")
+
+    //2 задание
+    println("Let me guess your age.\n" + "Enter remainders of dividing your age by 3, 5 and 7.")
+    val scan = Scanner(System.`in`)
+    val remainder3 = scan.nextInt()
+    val remainder5 = scan.nextInt()
+    val remainder7 = scan.nextInt()
+    val age = remainder3 * 70 + remainder5 * 21 + remainder7 * 15 % 105
+    println("Your age is $age!")
 
 
 }
