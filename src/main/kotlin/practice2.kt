51f203af35dd4e54dadcd7d924e2b6b6697aa419
perepelochka <o.o.perepelytsia@student.khai.edu>
2023-02-27 12:15:02 +0200

    3 task done

--- src/main/kotlin/practice2.kt
+++ src/main/kotlin/practice2.kt
@@ -12,15 +12,19 @@ fun main() {
         // Вызываем функцию, возвращающую "Even для четных чисел" или "Odd для нечетных чисел"
         val result = evenOrOdd(number)
         // Выводим результат
-        println("Результат 1: $result")
+        println("Задание 1: $result")
         // Вызываем функцию, умножающую число на 4 или 5 в зависимости от его четности
         val result2 = multiplyBy(number)
         // Выводим результат
-        println("Результат 2: $result2")
+        println("Задание 2: $result2")
+        // Вызываем функцию, возвращающую негативное число введенного значения
+        val result3 = negateNumber(number)
+        // Выводим результат
+        println("Задание 3: $result3")
     }
 }
 
-// Функция, возвращающая "Even" или "Odd" в зависимости от введенного числа
+// Функция, возвращающая "Even" или "Odd"
 fun evenOrOdd(number: Int): String {
     return if (number % 2 == 0) {
         "Even"
@@ -34,4 +38,8 @@ fun multiplyBy(number: Int): Int {
     } else {
         number * 5
     }
+}
+// Функция, возвращающая негативное число введенного значения
+fun negateNumber(number: Int): Int {
+    return -number
 }
